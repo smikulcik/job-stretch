a32e6410e75f87c75aed91395cb501c63c85a230
thomaswilson78 <thomas_wilson78@mymail.eku.edu>
2016-12-06 15:20:29 -0500

    Added DBTester to TestMain
    Added the (new DBTester()).testAll() line to DBTester to test the database functions.

--- src/test/java/com/tenaciouspanda/jobstretchtest/TestMain.java
+++ src/test/java/com/tenaciouspanda/jobstretchtest/TestMain.java
@@ -13,6 +13,7 @@ public class TestMain {
     public static void main(String[] args) {
         (new GeocoderTester()).testAll();
         (new SessionTester()).testAll();
+        (new DBTester()).testAll();
         System.out.println("All tests successful");
     }
 }
